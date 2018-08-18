aac59f5ba5e1ab3bd79e095d2ed67bee3fd80df0
Kevin Geng <kevin.geng@quarksys.ca>
2018-08-18 00:37:16 -0400

    dealer now plays against player. added basic end game scenarios

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # 21 Blackjack game
 import random
 import os
+import time
 
 """CONSTANTS"""
 # For now, Ace will be always be one
@@ -8,6 +9,10 @@ cardValues = {"Ace":1, "Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":
 # Overall, suits are nothing but I'll include them for the aesthetic for later
 suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
 BUSTED_TEXT_ART = "\n\t=======================\n\t‖       Busted!       ‖\n\t=======================\n"
+DEALER_BUST_TEXT_ART = "\n\t=======================\n\t‖     Dealer bust!    ‖\n\t=======================\n"
+
+
+""" FUNCTIONS: TOOLS """
 
 def clear():
 	if os.name == 'nt':
@@ -15,6 +20,7 @@ def clear():
 	if os.name == 'posix':
 		os.system('clear')
 
+""" FUNCTIONS: CARDS """
 # Generates a random card. Value, Suit
 def generateCard():
     return random.choice(list(cardValues.keys())), random.choice(suits)
@@ -27,12 +33,6 @@ def dealCards(p, d):
 
     displayCards(p, d)
 
-# Checks if player has busted. Will account for aces in future
-def isBust(hand): # TODO: Ace exception
-    if cardTotal(hand) > 21:
-        return True
-    else:
-        return False
 
 # Returns the value of the hand
 def cardTotal(hand):
@@ -43,6 +43,17 @@ def cardTotal(hand):
 
     return total
 
+
+# Checks if player has busted. Will account for aces in future
+def isBust(hand): # TODO: Ace exception
+    if cardTotal(hand) > 21:
+        return True
+    else:
+        return False
+
+
+
+""" FUNCTIONS: DISPLAY """
 # Prints the cards on the screen
 def displayCards(p, d):
     clear()
@@ -50,7 +61,7 @@ def displayCards(p, d):
     if isBust(p):
         print(BUSTED_TEXT_ART)
 
-    print("\nPlayer has :")
+    print("Player has :")
     for i in p:
         print(i[0]+" of "+ i[1])
     print("Total: "+ str(cardTotal(p)))
@@ -60,42 +71,97 @@ def displayCards(p, d):
         print(i[0]+" of "+ i[1])
     print("Total: "+ str(cardTotal(d)))
 
+
+def continueGame():
+    input("\n\nPress Enter to continue...")
+    menu()
+
+def playerWin():
+    # In future versions, this will do something with bets
+    print("\n\nPlayer Win. Congrats!")
+    continueGame()
+
+def dealerBust():
+    print(DEALER_BUST_TEXT_ART)
+    playerWin()
+
+def dealerWin():
+    # In future versions, this will do something with bets
+    print("\n\nDealer Win. Sorry!")
+    continueGame()
+
+def gameTie():
+    print("\n\nTied.")
+    continueGame()
+
+
+
+""" FUNCTIONS: ACTIONS """
 def actionHit(hand):
     hand.append(generateCard())
-    return 0
 
 
-def userAction(hand):
+
+""" FUNCTIONS: ACTIONS - USER """
+def userAction(hand, dHand):
     print("\nCommands: h = Hit\ts = Stand")
     userInput = input("Input:")
 
     if userInput == "h":
         actionHit(hand)
     elif userInput == "s":
-        print("stand")
+        dealerAction(dHand, hand)
     else:
         print("Invalid Input")
 
+
+
+""" FUNCTIONS: ACTIONS - DEALER """
+def dealerAction(hand, phand):
+    # Dealer must draw to 16, stand on 17
+    while cardTotal(hand) <= 16:
+        actionHit(hand)
+        displayCards(phand, hand)
+
+    dealerStand(hand, phand)
+
+
+def dealerStand(dhand, phand):
+    if cardTotal(dhand) > 21:
+        dealerBust()
+    elif cardTotal(phand) > cardTotal(dhand):
+        playerWin()
+    elif cardTotal(dhand) > cardTotal(phand):
+        dealerWin()
+    elif cardTotal(phand) == cardTotal(dhand):
+        gameTie()
+
+
+""" FUNCTIONS: MAIN """
 def playGame():
     playerCards = []
     dealerCards = []
 
+    dealCards(playerCards, dealerCards)
+
+    while not isBust(playerCards):
+        userAction(playerCards, dealerCards)
+        displayCards(playerCards, dealerCards)
+
+
+def menu():
     userInput = str()
+    clear()
+    print("Welcome to 21 Blackjack. Made by Kevin Geng.\n\n")
 
     while (userInput != "q"):
-        print("\nCommands: n = New Game\tq = Quit")
+        userInput = str()
+        print("Commands: n = New Game\tq = Quit")
         userInput = input("Input: ")
 
         if userInput == "n":
-            dealCards(playerCards, dealerCards)
-
-            while not isBust(playerCards):
-                userAction(playerCards)
-                displayCards(playerCards, dealerCards)
+            playGame()
 
 
 if __name__ == "__main__":
-    playGame()
-
-
-    # When this is integrated with the front end, there will be a start game button
+    menu()
